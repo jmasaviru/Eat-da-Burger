bd7a806e314d8de86e75c1fc5805f15d4c9ac6ca
James Masaviru <jmasaviru@gmail.com>
2021-02-12 07:00:29 -0500

    Updating seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-USE burger_db;
+USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Bacon Cheeseburger", false), ("Teriyaki Burger", true), ("Veggie Burger", false), ("Brazi Grill Burger", true), ("Juicy Lucy", false), ("Spicy Fish Chilli Mayo", true);
