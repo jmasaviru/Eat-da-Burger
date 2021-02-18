d754797f6b9c2bc85f58c29360c0694758779874
James Masaviru <jmasaviru@gmail.com>
2021-02-18 17:37:46 -0500

    Updating controller & removing delete function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,40 +18,26 @@ router.get("/", (req, res) => {
 
 //POST method
 router.post("/api/burgers", (req,res) => {
-    burger.insertOne('burger_name', [req.body.burger_name], result => {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], result => {
         console.log('Updated');
-        res.redirect("/");
+        res.json({id: result.insertId});
     });
 });
 
-// UPDATE method
+// PUT method
 router.put("/api/burgers/:id", (req,res) => {
-    var condition = "Id = " + req.params.id;
+    var condition = "id = " + req.params.id;
   
    console.log("condition", condition);
 
-   burger.updateOne({ devoured: true }, condition, (result) => {
-    if (result.changedRows == 0){
+   burger.update({ devoured: req.body.devoured }, condition, (result) => {
+    if (result.changedRows === 0){
          // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
     } else {
         res.status(200).end();
     }
   });
-});
-
-// DELETE method
-router.delete("/api/burgers/:id", (req,res) => {
-    var condition = "Id = " + req.params.id;
-
-    burger.deleteOne(condition, (result) => {
-        if (result.affectedRows == 0) {
-           // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  }); 
 });
 
 module.exports = router;
