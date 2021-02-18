73c858d509b37737efc26987df6dd33eb26da9e3
James Masaviru <jmasaviru@gmail.com>
2021-02-18 17:54:00 -0500

    Updating burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,25 @@
+// Import ORM to interact with DB
 var orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: (cb) => {
-      orm.selectAll("burgers", (res) => {
-        cb(res);
+  //Selecting all burgers
+  all: (cb) => {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // Creating a new burger 
+  create: (cols, vals, cb) => {
+      orm.insertOne("burgers", cols, vals, (res) => {
+          cb(res);
       });
-    },
-    // Inserting a new burger 
-    insertOne: (cols, vals, cb) => {
-        orm.insertOne("burgers", cols, vals, (res) =>{
-            cb(res);
-        });
-    },
-    // Updating a devoured burger
-    updateOne: (objColVals, condition, cb) => {
-        orm.updateOne("burgers", objColVals, condition, (res) => {
-          cb(res);
-        });
-    },
-    // Deleting a burger
-    deleteOne: (condition, cb) => {
-        orm.deleteOne("burgers", condition, (res) => {
-          cb(res);
-        });
-    },
+  },
+  // Updating a devoured burger
+  update: (objColVals, condition, cb) => {
+    orm.updateOne("burgers", objColVals, condition, (res) => {
+      cb(res);
+    });
+  },
 };
 
 module.exports = burger;
