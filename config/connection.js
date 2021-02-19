719d3fbe00fee3c115893cf118d247a4dfcfca44
James Masaviru <jmasaviru@gmail.com>
2021-02-19 15:05:11 -0500

    Updating config files

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,10 @@
 var mysql = require("mysql");
 
 // Connection to MYSQL db & Heroku connection
-var connection;
-
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
-    connection = mysql.createConnection({
+    var connection = mysql.createConnection({
       host: "localhost",
       port: 3306,
       user: "root",
--- config/orm.js
+++ config/orm.js
@@ -1,85 +1,77 @@
+// Import MYSQL connection
 var connection = require("../config/connection.js");
 
 // Helper functions for MySQL syntax
-function questionMarks(num) {
+function questionMarks (num) {
     var array = [];
     for (var i = 0; i < num; i++){
         array.push("?");
     }
     return array.toString();
 }
+
 // Helper function to convert Object key to SQL syntax
- function objToSql(ob) {
+ function objToSql(ob){
     var array = [];
     for (var key in ob) {
-        var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-               value = "'" + value + "'";
-            }
-            array.push(key + "=" + value + "'")
+      var value = ob[key];
+      // Skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)){
+        // Add quotes for spaced string
+        if (typeof value === "string" && value.indexOf(" ") >= 0){
+          value = "'" + value + "'";
         }
+        array.push(key + "=" + value + "'")
+      }
     }
+    // Array of strings translated into one string
     return array.toString()
-}
-// Object Relational Mapper
-var orm = {
-  selectAll: (tableInput, cb) => {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-
-  insertOne: (table, cols, vals, cb) => {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += questionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  updateOne: (table, objColVals, condition, cb) => {
-    var queryString ="UPDATE " + table;
+  }
+  // Object Relational Mapper
+  var orm = {
+    selectAll: (tableInput, cb) => {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    // Function that inserts new data in Burgers Table
+    insertOne: (table, cols, vals, cb) => {
+      var queryString = "INSERT INTO " + table;
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += questionMarks(vals.length);
+      queryString += ") ";
+      
+      console.log(queryString);
+      connection.query(queryString, vals, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    
+    updateOne: (table, objColVals, condition, cb) => {
+      var queryString ="UPDATE " + table;
       queryString += " SET ";
       queryString += objToSql(objColVals);
       queryString += " WHERE ";
       queryString += condition;
+
       console.log(queryString);
-      connection.query(queryString,(err, result) => {
+      connection.query(queryString, (err, result) => {
         if (err) {
           throw err;
         }
         cb(result);
       });
-    },
-    deleteOne: function (table, condition, cb) {
-      var queryString = "DELETE FROM " + table;
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      connection.query(queryString, function (err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-    },
+    }
   };
 
 module.exports = orm;
