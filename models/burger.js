9014ed80d573330623427b041ed3280fad1bb820
James Masaviru <jmasaviru@gmail.com>
2021-02-20 08:49:50 -0500

    Updating model orm functions

--- models/burger.js
+++ models/burger.js
@@ -4,22 +4,31 @@ var orm = require("../config/orm.js");
 var burger = {
   //Selecting all burgers
   all: (cb) => {
-    orm.selectAll("burgers", (res) => {
+    orm.all("burgers", (res) => {
       cb(res);
     });
   },
+
   // Creating a new burger 
   create: (cols, vals, cb) => {
-      orm.insertOne("burgers", cols, vals, (res) => {
+      orm.create("burgers", cols, vals, (res) => {
           cb(res);
       });
   },
+
   // Updating a devoured burger
   update: (objColVals, condition, cb) => {
-    orm.updateOne("burgers", objColVals, condition, (res) => {
+    orm.update("burgers", objColVals, condition, (res) => {
       cb(res);
     });
   },
+
+  // Deleting a burger 
+  delete: (condition, cb) => {
+    orm.delete("burgers", condition, (res) => {
+      cb(res);
+    });
+  }
 };
 
 module.exports = burger;
