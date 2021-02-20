c217fadbf03864ea7d8839c5c4a5a45392df13c3
James Masaviru <jmasaviru@gmail.com>
2021-02-20 08:33:18 -0500

    Adding route to remove burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,10 +5,10 @@ const router = express.Router();
 // Importing burger.js Model
 const burger = require("../models/burger.js");
 
-// Creating the routes 
+// Creating the routes for burgers
 // GET method
 router.get("/", (req, res) => {
-    burger.selectAll((data) => {
+    burger.all((data) => {
         console.log(data);
         var hbsObject = {burgers: data};
         console.log(hbsObject);
@@ -16,26 +16,39 @@ router.get("/", (req, res) => {
   });
 });
 
-//POST method
-router.post("/api/burgers", (req,res) => {
-    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], result => {
+//POST method for new burger in the menu
+router.post("/", (req,res) => {
+    burger.create(["burger_name"], [req.body.burger_name], (result) => {
         console.log('Updated');
-        res.json({id: result.insertId});
+        res.redirect("/");
     });
 });
 
-// PUT method
-router.put("/api/burgers/:id", (req,res) => {
+// PUT method to update menu
+router.put("/:id", (req,res) => {
     var condition = "id = " + req.params.id;
   
    console.log("condition", condition);
 
    burger.update({ devoured: req.body.devoured }, condition, (result) => {
-    if (result.changedRows === 0){
+    if (result.changedRows == 0){
          // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
-    } else {
+      } else {
         res.status(200).end();
+      }
+   });
+});
+
+// Delete Burger from menu
+router.delete("/api/burgers/:id", (req, res) => {
+  var condition = "id = " + req.params.id;
+
+  burgers.delete(condition, (result) => {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
   });
 });
