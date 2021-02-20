d97cf74e1112b03a9173ba2855175c377a3f9f41
James Masaviru <jmasaviru@gmail.com>
2021-02-20 08:14:44 -0500

    Updating server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-// Use Express
+// Use Express, Handlebars & MySQL
 const express = require('express');
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Set up the public directory for the app
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
 // Parse JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Use Handlebars
-const exphbs = require("express-handlebars");
 
 // Set handlebars as main HTML file
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
