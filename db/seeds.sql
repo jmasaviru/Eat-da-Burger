bafa5ab02de36a884eb721459efa2f62294c3088
James Masaviru <jmasaviru@gmail.com>
2021-02-23 04:10:11 -0500

    Adding to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("Bacon Cheeseburger", false), ("Teriyaki Burger", true), ("Veggie Burger", false), ("Brazil Grill Burger", true), ("Juicy Lucy", false), ("Spicy Fish Chilli Mayo", true);
+VALUES ("Bacon Cheeseburger", false), ("Teriyaki Burger", true), ("Veggie Burger", false), ("Brazil Grill Burger", true), ("Juicy Lucy", false), ("Buffallo & Onion Fries Burger", false), ("Spicy Fish Chilli Mayo", true);
