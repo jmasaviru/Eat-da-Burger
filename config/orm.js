63859433402bf91a34a643acec1cab86c1df80ae
James Masaviru <jmasaviru@gmail.com>
2021-02-24 05:56:58 -0500

    Updating ORM functions to update burger

--- config/orm.js
+++ config/orm.js
@@ -1,94 +1,47 @@
 // Import MYSQL connection
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
-// Helper functions for MySQL syntax
-function questionMarks(num) {
-    var arr = [];
-    for (var i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
+// Object Relational Mapper
+var orm = {
+// Function to select all MySQL queries
+all: (tableInput, cb) => {
+  var queryAll = `SELECT * FROM ${tableInput};`
+  connection.query(queryAll, (err, result) => {
+   // console.log(result);
+    if (err) throw err;
+    cb(result);
+})
+},
+
+// Function that creates new Burgers in menu
+create: (tableInput, name, cb) => {
+  var queryCreate = `INSERT INTO ${tableInput} (burger_name) VALUES ('${name}');`
+  connection.query(queryCreate, (err, result)=>{
+    // console.log(result);
+    if (err) throw err;
+    cb(result);
+})
+},
+
+// Function to update burger as 'devoured'
+update: (tableInput, id, devoured, cb)=> {
+  var queryUpdate = `UPDATE ${tableInput} SET devoured = ${devoured}  WHERE id = ${id};`
+  connection.query(queryUpdate, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    cb(result);
+  })
+},
+
+// Function to delete burger
+delete: (tableInput, id, cb)=>{
+  var queryDelete = `DELETE FROM ${tableInput} WHERE id = ${id};`
+  connection.query(queryDelete, (err, result) => {
+      if (err) throw err;
+      cb(result);
+  })
 }
+};
 
-// Helper function to convert Object key to SQL syntax
- function objToSql(ob){
-    var arr = [];
-    for (var key in ob) {
-      var value = ob[key];
-
-      // Skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)){
-        // Add quotes for spaced string
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        arr.push(key + "=" + value)
-      }
-    }
-    // Array of strings translated into one string
-    return array.toString()
-  }
-  // Object Relational Mapper
-  var orm = {
-    all: (tableInput, cb) => {
-      var queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, (err, result) => {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    // Function that creates new Burgers in menu
-    create: (table, cols, vals, cb) => {
-      var queryString = "INSERT INTO " + table;
-      queryString += " (";
-      queryString += cols.toString();
-      queryString += ") ";
-      queryString += "VALUES (";
-      queryString += questionMarks(vals.length);
-      queryString += ") ";
-      
-      console.log(queryString);
-      connection.query(queryString, vals, (err, result) => {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    
-    // Function to update existing burger
-    update: (table, objColVals, condition, cb) => {
-      var queryString ="UPDATE " + table;
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
-
-      console.log(queryString);
-      connection.query(queryString, (err, result) => {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-
-    // Function to delete burger
-    delete: (table, condition, cb) => {
-      var queryString = "DELETE FROM " + table;
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      connection.query(queryString, (err, result) => {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-    }
-  };
 
 module.exports = orm;
