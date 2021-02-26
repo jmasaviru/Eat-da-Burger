e2f9c143b2be9a4f5d40e38f722e6741da65f39b
James Masaviru <jmasaviru@gmail.com>
2021-02-26 12:20:50 -0500

    Updating host as 'localhost'

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
     var connection = mysql.createConnection({
-      host: "burgers_db",
+      host: "localhost",
       port: 3306,
       user: "root",
       password: "Ma$terviru6",
